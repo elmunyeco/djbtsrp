87bfb8862f9034f1a09df72900e449ee8a348681
elmunyeco <elyecomun@gmail.com>
2021-10-01 19:11:35 -0300

    removed asterisks from crysp form fields in patients crud form

--- venvprj/pt_crud/forms.py
+++ venvprj/pt_crud/forms.py
@@ -6,5 +6,18 @@ class PatientForm(forms.ModelForm):
 
     class Meta:
         model = Paciente
-        #fields = ("",)
+        # fields = ("",)
         fields = '__all__'
+        labels =   {
+            'nacimiento_fecha':'Fecha de Nacimiento',
+            'documento_numero':'Número de Documento',
+            'documento_tipo':'Tipo de Documento',
+            'genero':'Género',
+            'direccion':'Dirección',
+            'obra_social':'Obra Social',
+            'afiliado':'Identificación como Afiliado',
+            'telefono_1':'Línea Telefónica',
+            'telefono_2':'Línea Alternativa',
+            'profesion':'Profesión',
+            'referente':'Profesional Referente'
+        }
