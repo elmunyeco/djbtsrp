43981b1928ebb5d8361291c41c21c79a4e1d1fb5
elmunyeco <elyecomun@gmail.com>
2021-10-06 20:36:36 -0300

    First list and cr!!!!

--- venvprj/pt_crud/views.py
+++ venvprj/pt_crud/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from .forms import PacienteForm 
+from .models import Paciente
 
 # Create your views here.
 
 def paciente_list(request):
-    return render(request,'pt_crud/paciente_list.html')
+    context = {'paciente_list':Paciente.objects.all()}
+    return render(request,'pt_crud/paciente_list.html',context)
 
 def paciente_form(request):
     if request.method == "GET":
